a55743648b2d5039a4c68b256c089b58fd3060a1
vichsu <vic.hsu@silkrode.com.tw>
2020-07-18 16:05:35 +0800

    add echoproto pkg which is protobuf for publishing data to websocket server

--- pkg/echoproto/proto_test.go
+++ pkg/echoproto/proto_test.go
@@ -1,34 +1,98 @@
 package echoproto
 
 import (
+	"encoding/json"
 	"testing"
 
-	"github.com/golang/protobuf/proto"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/proto"
 )
 
-func TestCommandProto(t *testing.T) {
-	room := &Room{
-		Name: "test",
+// func TestCommandProto(t *testing.T) {
+// 	room := &Room{
+// 		Name: "test",
+// 	}
+// 	data, err := proto.Marshal(room)
+// 	assert.Nil(t, err)
+
+// 	command := &Command{
+// 		Type:    CommandType_CreateRoom,
+// 		Payload: data,
+// 	}
+
+// 	data, err = proto.Marshal(command)
+// 	assert.Nil(t, err)
+
+// 	command2 := &Command{}
+// 	err = proto.Unmarshal(data, command2)
+// 	assert.Nil(t, err)
+// 	assert.Equal(t, command2.Type, command.Type)
+
+// 	room2 := &Room{}
+// 	err = proto.Unmarshal(command2.Payload, room2)
+// 	assert.Nil(t, err)
+// 	assert.Equal(t, room2.Name, room.Name)
+// }
+
+func TestChiness(t *testing.T) {
+	msg := &Message{
+		Uid:  "12345678901234567890",
+		Text: "你好嗎，我很好，你好嗎，我很好，你好嗎，我很好，你好嗎，我很好，你好嗎，我很好，你好嗎，我很好，你好嗎，我很好，你好嗎，我很好，",
+		Type: MessageType_Text,
+		Sender: &User{
+			Id:   1,
+			Name: "隔壁的小王",
+		},
 	}
-	data, err := proto.Marshal(room)
+	data, err := proto.Marshal(msg)
 	assert.Nil(t, err)
+	msg2 := &Message{}
 
-	command := &Command{
-		Type:    CommandType_CreateRoom,
-		Payload: data,
+	err = proto.Unmarshal(data, msg2)
+	assert.Nil(t, err)
+	assert.Equal(t, msg.Text, msg2.Text)
+}
+
+func BenchmarkProto(b *testing.B) {
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		msg := &Message{
+			Uid:  "12345678901234567890",
+			Text: "你好嗎，我很好，你好嗎，我很好，你好嗎，我很好，你好嗎，我很好，你好嗎，我很好，你好嗎，我很好，你好嗎，我很好，你好嗎，我很好，",
+			Type: MessageType_Text,
+			Sender: &User{
+				Id:   1,
+				Name: "隔壁的小王",
+			},
+			Room: &Room{
+				Uid: "14293403902afkasdlgk23423fsdf",
+			},
+		}
+		data, _ := proto.Marshal(msg)
+		msg2 := &Message{}
+		proto.Unmarshal(data, msg2)
 	}
+}
 
-	data, err = proto.Marshal(command)
-	assert.Nil(t, err)
+func BenchmarkJson(b *testing.B) {
+	b.ResetTimer()
 
-	command2 := &Command{}
-	err = proto.Unmarshal(data, command2)
-	assert.Nil(t, err)
-	assert.Equal(t, command2.Type, command.Type)
-
-	room2 := &Room{}
-	err = proto.Unmarshal(command2.Payload, room2)
-	assert.Nil(t, err)
-	assert.Equal(t, room2.Name, room.Name)
+	for i := 0; i < b.N; i++ {
+		msg := &Message{
+			Uid:  "12345678901234567890",
+			Text: "你好嗎，我很好，你好嗎，我很好，你好嗎，我很好，你好嗎，我很好，你好嗎，我很好，你好嗎，我很好，你好嗎，我很好，你好嗎，我很好，",
+			Type: MessageType_Text,
+			Sender: &User{
+				Id:   1,
+				Name: "隔壁的小王",
+			},
+			Room: &Room{
+				Uid: "14293403902afkasdlgk23423fsdf",
+			},
+		}
+		data, _ := json.Marshal(msg)
+		msg2 := &Message{}
+		json.Unmarshal(data, msg2)
+	}
 }
