e4a09e23aa9495534123c28556db8c5c3b580f4e
vichsu <vic.hsu@silkrode.com.tw>
2020-07-25 22:42:06 +0800

    update msg_svc pkg: fix joinRoom when bind new room to nats subject, if join not exist room

--- internal/pkg/app/service/msg_svc.go
+++ internal/pkg/app/service/msg_svc.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 
+	"github.com/nats-io/nats.go"
 	"github.com/rs/zerolog/log"
 	"github.com/whale-team/whaleEcho/internal/pkg/app/entity"
 	"github.com/whale-team/whaleEcho/pkg/natspool"
@@ -27,10 +28,14 @@ func (svc service) JoinRoom(ctx context.Context, roomUID string, user *entity.Us
 		log.Warn().Msg("join not existed room")
 		room := entity.NewRoom()
 		room.UID = roomUID
+		if err := svc.bindRoom(ctx, room); err != nil {
+			return err
+		}
+
 		if err := svc.rooms.AddRoom(room); err != nil {
 			return err
 		}
-		log.Warn().Msgf("add room(%s", roomUID)
+		log.Warn().Msgf("add room(%s)", roomUID)
 	}
 
 	if err := svc.rooms.JoinRoom(roomUID, user); err != nil {
@@ -39,8 +44,33 @@ func (svc service) JoinRoom(ctx context.Context, roomUID string, user *entity.Us
 	return nil
 }
 
+func (svc service) bindRoom(ctx context.Context, room *entity.Room) error {
+	msgCh := make(chan *nats.Msg, 1)
+	room.SetMsgChannel(msgCh)
+
+	for i := 0; i < 3; i++ {
+		sub, err := svc.msgBroker.BindChannelWithSubject(ctx, room.Subject(), msgCh)
+		if err != nil {
+			if wserrors.Is(err, natspool.ErrGetConnTimeout) {
+				continue
+			}
+			return wserrors.Wrapf(wserrors.ErrInternal, "err:%+v", err)
+		}
+		room.Subscribe = sub
+		break
+	}
+
+	return nil
+}
+
 // LeaveRoom method used to remove user from a specific room
 func (svc service) LeaveRoom(ctx context.Context, roomUID string, user *entity.User) error {
 	svc.rooms.LeaveRoom(roomUID, user)
+	return nil
+}
+
+// LeaveAllRooms leave all rooms when user close the connection
+func (svc service) LeaveAllRooms(ctx context.Context, user *entity.User) error {
+	svc.rooms.LeaveAllRooms(user)
 	return nil
 }
